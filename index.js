7600b3751b304ff5de3c64f298b4a0e09cf29096
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-05-17 03:46:20 +0600

    created a simple server

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//mongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctors-portal-backend.lkqdf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect()
+        const serviceCollection = client.db("doctors-portal").collection("services")
+        // uploading the data from mongoDB to server
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send("This is the server of doctors portal project")
+})
+
+app.listen(port, () => {
+    console.log('listening to port', port)
+})
